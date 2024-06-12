a99fc6ae58c8658975c2fb9ba4f7e69512521760
Fabio Kubo <fabio_harada@hotmail.com>
2024-06-12 16:51:50 -0300

    feat: implement manual command

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pandasai.llm import BambooLLM
 import os
 
 from auto_parser import AutoParser
+from manual_parser import ManualParser
 
 import typer
 
@@ -13,13 +14,6 @@ from typing_extensions import Annotated
 def make_agent(dfs: list[pd.DataFrame], llm) -> Agent:
 	return Agent(dfs, config={"llm": llm, "enforce_privacy": True})
 
-'''
-openai = OpenAI(api_token=os.getenv("OPENAI_API_TOKEN", default=""))
-bamboo = BambooLLM(api_key=os.getenv("BAMBOO_API_KEY", default=""))
-
-openai_agent = make_agent(t3, openai)
-bamboo_agent = make_agent(t3, bamboo)
-'''
 
 app = typer.Typer()
 
@@ -28,6 +22,15 @@ suppress_stderr()
 from enums import AvailableLLMs
 
 from llms import make_agent
+
+def main_loop(agent: Agent):
+	while True:
+		question = typer.prompt("Ask me anything")
+		answer = agent.chat(question)
+		if "Unfortunately, I was not able to answer your question" in answer:
+			print("Unfortunately, I was not able to answer your question.")
+		else:
+			print(answer)
 
 
 @app.command()
@@ -39,19 +42,43 @@ def auto(
 	parser = AutoParser()
 	tables = parser.parse_xlsx(file_path)
 	agent = make_agent(tables, llm)
+	main_loop(agent)
 
-	while True:
-		question = typer.prompt("Ask me anything")
-		answer = agent.chat(question)
-		if "Unfortunately, I was not able to answer your question" in answer:
-			print("Unfortunately, I was not able to answer your question.")
-		else:
-			print(answer)
 
+from manual_parser.types import TableInfo
+from typing import Optional
+
+def ask_for_table_info(sheet_name: Optional[str] = None):
+	if sheet_name:
+		use_previous_sheet_name = typer.confirm(f"Should use the previous sheet name ({sheet_name})?")
+		if not use_previous_sheet_name:
+			sheet_name = None
+
+	if not sheet_name:
+		sheet_name = typer.prompt("What is the sheet the table is?")
+	topleft_cell = typer.prompt("What is the top left cell the table is in?")
+	num_rows = int(typer.prompt("How many rows the table have?"))
+	num_cols = int(typer.prompt("How many columns the table have?"))
+	return TableInfo(sheet_name, topleft_cell, num_rows, num_cols)
 
 @app.command()
-def manual(file_path: str):
-	pass
+def manual(
+	file_path: Annotated[str, typer.Argument(help="The path to XLSX file to parse the tables")],
+	llm: Annotated[AvailableLLMs, typer.Argument(help="The LLM you want to answer your questions")] = AvailableLLMs.BAMBOO
+):
+	print("I'll need to collect some information from the file...")
+	tables_info = [ask_for_table_info()]
+	last_sheet_name = tables_info[0].sheet_name
+	has_another_table_to_add = typer.confirm("Do you have any other table to add?")
+
+	while has_another_table_to_add:
+		tables_info.append(ask_for_table_info(last_sheet_name))
+		has_another_table_to_add = typer.confirm("Do you have any other table to add?")
+
+	parser = ManualParser()
+	tables = parser.parse_xlsx(file_path, tables_info)
+	agent = make_agent(tables, llm)
+	main_loop(agent)
 
 if __name__ == "__main__":
 	app()
--- manual_parser/functions.py
+++ manual_parser/functions.py
@@ -3,14 +3,17 @@ import pandas as pd
 from .types import TableInfo
 
 def pick_table(sheet: Worksheet, table_info: TableInfo) -> pd.DataFrame:
-    topleft, num_rows, num_columns = table_info.topleft, table_info.num_rows, table_info.num_columns
+    topleft, num_rows, num_columns = table_info.topleft_cell, table_info.num_rows, table_info.num_cols
     topleft_cell = sheet[topleft]
 
     header = pick_content_from_one_row(sheet, topleft_cell.row, topleft_cell.column, num_columns)
     content = [pick_content_from_one_row(sheet, topleft_cell.row + i, topleft_cell.column, num_columns) for i in range(1, num_rows)]
 
     df = pd.DataFrame(content, columns=header)
-    return clear_nan_columns(clear_nan_rows(df))
+    df = clear_nan_columns(clear_nan_rows(df))
+    df.columns = [str(col) for col in df.columns]
+    return df
+
 
 def pick_content_from_one_row(sheet: Worksheet, row_index: int, start_column_index: int, num_columns: int) -> list:
     return [sheet.cell(row_index, start_column_index + i).value for i in range(num_columns)]
