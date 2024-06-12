ab578eb3bb367b0d9684e89c424b7a41d04f53ac
Fabio Kubo <fabio_harada@hotmail.com>
2024-06-12 14:57:10 -0300

    refactor: change auto parser to deal with sheets one by one

--- auto_parser/__init__.py
+++ auto_parser/__init__.py
@@ -1,7 +1,14 @@
 import pandas as pd
 from .functions import parse_tables_from_df
+from openpyxl import load_workbook
 
 class AutoParser:
 	def parse_xlsx(self, file_path: str) -> list[pd.DataFrame]:
-		df = pd.read_excel(file_path, header=None)
-		return parse_tables_from_df(df)
+		wb = load_workbook(file_path, data_only=True)
+		
+		tables = []
+		for sheet_name in wb.sheetnames:
+			tables_from_sheet = parse_tables_from_df(pd.DataFrame(wb[sheet_name].values))
+			tables.extend(tables_from_sheet)
+
+		return tables
